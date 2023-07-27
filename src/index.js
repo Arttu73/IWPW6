ee3db444dae9931782ed21003bf86a568b913bc6
Arttu Laitinen <124583634+Arttu73@users.noreply.github.com>
2023-07-27 15:02:22 +0000

    Pull

--- src/index.js
+++ src/index.js
@@ -1,10 +1,77 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const queryJson = {
+  "query": [
+      {
+          "code": "Vuosi",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "2000",
+                  "2001",
+                  "2002",
+                  "2003",
+                  "2004",
+                  "2005",
+                  "2006",
+                  "2007",
+                  "2008",
+                  "2009",
+                  "2010",
+                  "2011",
+                  "2012",
+                  "2013",
+                  "2014",
+                  "2015",
+                  "2016",
+                  "2017",
+                  "2018",
+                  "2019",
+                  "2020",
+                  "2021"
+              ]
+          }
+      },
+      {
+          "code": "Alue",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "SSS"
+              ]
+          }
+      },
+      {
+          "code": "Tiedot",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "vaesto"
+              ]
+          }
+      }
+  ],
+  "response": {
+      "format": "json-stat2"
+  }
+}
+
+const getData = async () => {
+  try {
+    const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
+    
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify(queryJson)
+    })
+
+    if(!res.ok) {return;}
+
+    const data = await res.json();
+    return data;
+
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+}
